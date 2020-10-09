94e50b5b7199022e5121275cbed78bfd44da36e5
Josh Glantz-Hucks <jahteo@gmail.com>
2020-10-09 09:15:15 -0700

    server is alive and set up

--- data/migrations/20201009082501_init.js
+++ data/migrations/20201009082501_init.js
@@ -1,8 +1,58 @@
+const { table } = require("console");
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments();
+      tbl.text("project_name").notNullable();
+      tbl.text("description");
+      tbl.boolean("completed").notNullable().defaultTo(false);
+    })
+
+    .createTable("resources", tbl => {
+      tbl.increments();
+      tbl.text("resource_name").notNullable();
+      tbl.text("description");
+      tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('projects')
+      .onUpdate('RESTRICT')
+      .onDelete('CASCADE');
+    })
+
+    .createTable("tasks", tbl => {
+      tbl.increments();
+      tbl.text("task_name").notNullable();
+      tbl.text("description").notNullable();
+      tbl.text("notes");
+      tbl.boolean("completed").notNullable().defaultTo(false);
+    })
+
+    .createTable("project_tasks", tbl => {
+      tbl.increments();
+      tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('projects')
+      .onUpdate('RESTRICT')
+      .onDelete('CASCADE');
+      tbl.integer('task_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('tasks')
+      .onUpdate('RESTRICT')
+      .onDelete('CASCADE');
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("project_tasks")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects")
 };
