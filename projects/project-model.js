740dfa071c79b36ad6ce6a13775af0dbb260c35f
Josh Glantz-Hucks <jahteo@gmail.com>
2020-10-09 10:50:53 -0700

    POSTing tasks is fighting back. I'm wrestling with how to send the data I get to multiple tables (specifically the tasks & project_tasks) in a single post. esp bc I need the response from the task insert to pull the id that the project_tasks insert needs. ugg.

--- projects/project-model.js
+++ projects/project-model.js
@@ -7,9 +7,13 @@ module.exports = {
   getAllTasks,
   addProject,
   addResource,
+  addTask,
+  addProjectTask,
   getByProjectId,
-  //getAllProjectsSteps,
+  getByResourceId,
+  getByTaskId,
   // remove,
+  //update
 };
 
 function getAllProjects() {
@@ -32,6 +36,10 @@ function getByResourceId(id) {
   return db("resources").where({ id }).first();
 }
 
+function getByTaskId(id) {
+  return db("tasks").where({ id }).first();
+}
+
 function addProject(data) {
   return db('projects').insert(data, "id")
   .then(ids => {
@@ -46,4 +54,27 @@ function addResource(data) {
     const id = ids[0];
     return getByResourceId(id);
   })
+}
+
+function addTask({ task_name, description, completed, notes, project_id }) {
+  const taskData = { task_name, description, completed, notes }
+  return db('tasks').insert(taskData, "id")
+  .then(ids => {
+    const task_id = ids[0];
+    // const project_tasksData = { project_id, task_id }
+    // console.log(project_tasksData)
+    // db('project_tasks'),insert(project_tasksData, "id")
+    return getByTaskId(task_id);
+  })
+}
+
+function addProjectTask( project_id, task_id ) {
+  const project_tasksData = { project_id, task_id }
+  return db('project_tasks').insert(project_tasksData, "id")
+  // .then(ids => {
+  //   const task_id = ids[0];
+  //   // console.log(project_tasksData)
+  //   // db('project_tasks'),insert(project_tasksData, "id")
+  //   return getByTaskId(task_id);
+  // })
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -29,6 +29,22 @@ router.post("/:id/resource", (req, res) => {
     });
 });
 
+//post a new task
+router.post("/:id/tasks", (req, res) => {
+  const project_id = req.params.id;
+  Projects.addTask(req.body)
+    .then(task => {
+      const task_id = task.id
+      // console.log(req.body)
+      res.status(201).json({ created: task });
+      Projects.addProjectTask(project_id, task_id)
+    })
+    .catch(err => {
+      console.log(req.body)
+      res.status(500).json({ message: "Failed to create new task" });
+    });
+});
+
 //get all projects
 router.get('/', (req, res) => {
   Projects.getAllProjects()
@@ -40,6 +56,7 @@ router.get('/', (req, res) => {
   });
 });
 
+//get all resources
 router.get('/resources', (req, res) => {
   Projects.getAllResources()
   .then(projects => {
@@ -47,6 +64,17 @@ router.get('/resources', (req, res) => {
   })
   .catch(err => {
     res.status(500).json({ message: 'Failed to get projects' });
+  });
+});
+
+//get all tasks
+router.get('/tasks', (req, res) => {
+  Projects.getAllTasks()
+  .then(tasks => {
+    res.json(tasks);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get tasks' });
   });
 });
 
