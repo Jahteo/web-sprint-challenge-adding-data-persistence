673b033025368c4226ca128690d94f4ec8fb2235
Josh Glantz-Hucks <jahteo@gmail.com>
2020-10-09 09:55:41 -0700

    successfully able to POST resources & GET a list of them back

--- projects/project-model.js
+++ projects/project-model.js
@@ -3,7 +3,10 @@ const db = require('../data/db-config.js');
 
 module.exports = {
   getAllProjects,
+  getAllResources,
+  getAllTasks,
   addProject,
+  addResource,
   getByProjectId,
   //getAllProjectsSteps,
   // remove,
@@ -12,8 +15,21 @@ module.exports = {
 function getAllProjects() {
   return db('projects');
 };
+
+function getAllResources() {
+  return db('resources');
+};
+
+function getAllTasks() {
+  return db('tasks');
+};
+
 function getByProjectId(id) {
   return db("projects").where({ id }).first();
+}
+
+function getByResourceId(id) {
+  return db("resources").where({ id }).first();
 }
 
 function addProject(data) {
@@ -21,5 +37,13 @@ function addProject(data) {
   .then(ids => {
     const id = ids[0];
     return getByProjectId(id);
+  })
+}
+
+function addResource(data) {
+  return db('resources').insert(data, "id")
+  .then(ids => {
+    const id = ids[0];
+    return getByResourceId(id);
   })
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -5,6 +5,7 @@ const Projects = require('./project-model.js');
 const router = express.Router();
 
 //Routes
+//post a new project
 router.post("/", (req, res) => {
   const projectData = req.body;
   Projects.addProject(projectData)
@@ -16,8 +17,31 @@ router.post("/", (req, res) => {
     });
 });
 
+//post a new resource
+router.post("/:id/resource", (req, res) => {
+  req.body.project_id = req.params.id;
+  Projects.addResource(req.body)
+    .then(resource => {
+      res.status(201).json({ created: resource });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to create new resource" });
+    });
+});
+
+//get all projects
 router.get('/', (req, res) => {
   Projects.getAllProjects()
+  .then(projects => {
+    res.json(projects);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get projects' });
+  });
+});
+
+router.get('/resources', (req, res) => {
+  Projects.getAllResources()
   .then(projects => {
     res.json(projects);
   })
